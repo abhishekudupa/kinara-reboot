d18f8d23582dbd5f060c16707052bb95693e3bd1
Abhishek Udupa <audupa@seas.upenn.edu>
2015-03-16 10:55:40 -0400

    Added perf tests for prio queue. 4-ary queues seem to perform best

--- tests/unit-tests/PriorityQueueTests.cpp
+++ tests/unit-tests/PriorityQueueTests.cpp
@@ -36,6 +36,7 @@
 // Code:
 
 #include "../../projects/kinara-common/src/containers/PriorityQueue.hpp"
+#include "../../projects/kinara-common/src/containers/Vector.hpp"
 #include "../../projects/kinara-common/src/containers/MultiWayHeap.hpp"
 #include "../../projects/kinara-common/src/containers/String.hpp"
 
@@ -66,6 +67,7 @@ using kinara::containers::BinaryHeap;
 using kinara::containers::TernaryHeap;
 using kinara::containers::QuaternaryHeap;
 using kinara::containers::MultiWayHeap;
+using kinara::containers::Vector;
 
 using testing::Types;
 
@@ -75,6 +77,33 @@ class PrioQueueTest : public testing::Test
 protected:
     PrioQueueTest() {}
     virtual ~PrioQueueTest() {}
+};
+
+#define PERF_TEST_TEST_SIZE ((u64)(1 << 20))
+#define PERF_TEST_ITERATIONS ((u64)(1 << 6))
+
+template <typename PrioQueueType>
+class PrioQueuePerfTest : public testing::Test
+{
+protected:
+    Vector<i64> m_random_data;
+
+    PrioQueuePerfTest()
+        : m_random_data((size_t)PERF_TEST_TEST_SIZE, 0)
+    {
+        std::default_random_engine generator;
+        std::uniform_int_distribution<i64> distribution(0, 1 << 30);
+
+        for (u64 i = 0; i < PERF_TEST_TEST_SIZE; ++i) {
+            auto elem = distribution(generator);
+            m_random_data[i] = elem;
+        }
+    }
+
+    virtual ~PrioQueuePerfTest()
+    {
+        // Nothing here
+    }
 };
 
 class i64i64PairCompare
@@ -88,6 +117,7 @@ public:
 };
 
 TYPED_TEST_CASE_P(PrioQueueTest);
+TYPED_TEST_CASE_P(PrioQueuePerfTest);
 
 TYPED_TEST_P(PrioQueueTest, Constructor)
 {
@@ -173,10 +203,34 @@ TYPED_TEST_P(PrioQueueTest, Functional)
     }
 }
 
+TYPED_TEST_P(PrioQueuePerfTest, PerfTest)
+{
+    typedef TypeParam PrioQueueT;
+
+    PrioQueueT prio_queue;
+
+    for (u64 j = 0; j < PERF_TEST_ITERATIONS; ++j) {
+
+        for (u64 i = 0; i < PERF_TEST_TEST_SIZE; ++i) {
+            prio_queue.push(i);
+        }
+
+        EXPECT_EQ(PERF_TEST_TEST_SIZE, prio_queue.size());
+
+        for (u64 i = 0; i < PERF_TEST_TEST_SIZE; ++i) {
+            prio_queue.pop();
+        }
+
+        EXPECT_EQ(0ul, prio_queue.size());
+    }
+}
 
 REGISTER_TYPED_TEST_CASE_P(PrioQueueTest,
                            Constructor,
                            Functional);
+
+REGISTER_TYPED_TEST_CASE_P(PrioQueuePerfTest,
+                           PerfTest);
 
 typedef Types<PriorityQueue<std::pair<i64, i64>,
                             i64i64PairCompare,
@@ -189,8 +243,21 @@ typedef Types<PriorityQueue<std::pair<i64, i64>,
                             QuaternaryHeap<std::pair<i64, i64> > > >
 PriorityQueueImplementations;
 
+typedef Types<PriorityQueue<i64, std::less<i64>, BinaryHeap<i64> >,
+              PriorityQueue<i64, std::less<i64>, TernaryHeap<i64> >,
+              PriorityQueue<i64, std::less<i64>, QuaternaryHeap<i64> >,
+              PriorityQueue<i64, std::less<i64>, MultiWayHeap<i64, std::less<i64>, 5> >,
+              PriorityQueue<i64, std::less<i64>, MultiWayHeap<i64, std::less<i64>, 6> >,
+              PriorityQueue<i64, std::less<i64>, MultiWayHeap<i64, std::less<i64>, 7> >,
+              PriorityQueue<i64, std::less<i64>, MultiWayHeap<i64, std::less<i64>, 8> >,
+              std::priority_queue<i64, std::vector<i64>, std::greater<i64> > >
+PriorityQueuePerfImplementations;
+
 INSTANTIATE_TYPED_TEST_CASE_P(PrioQueueTemplateTests,
                               PrioQueueTest, PriorityQueueImplementations);
+
+INSTANTIATE_TYPED_TEST_CASE_P(PrioQueuePerfTests,
+                              PrioQueuePerfTest, PriorityQueuePerfImplementations);
 
 //
 // PriorityQueueTests.cpp ends here
